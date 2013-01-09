4f299435390bd7e2c50805814867c5b72abbac64
TJ Holowaychuk <tj@vision-media.ca>
2013-01-09 09:18:22 -0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+
+/**
+ * Module dependencies.
+ */
+
+var spawn = require('child_process').spawn
+  , debug = require('debug')('cloudup:highlight');
+
+/**
+ * Highlight the given code `str` as `lang`
+ * and invoke `fn(err, html)`.
+ *
+ * @param {String} str
+ * @param {String} lang
+ * @param {Function} fn
+ * @api public
+ */
+
+module.exports = function(str, lang, fn){
+  var buf = '';
+  debug('highlight %s', lang);
+  var proc = spawn('pygmentize', ['-l', lang, '-f', 'html', '-O', 'encoding=utf8,linenos=true']);
+  proc.stdout.setEncoding('utf8');
+  proc.stdout.on('data', function(s){ buf += s });
+  proc.on('exit', function(){ fn(null, buf) });
+  proc.stdin.write(str);
+  proc.stdin.end();
+};
